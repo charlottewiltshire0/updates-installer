5f22790a4a05a9dc35851c459961138045ef8054
StarStudio <charlottewiltshire27@outlook.com>
2024-07-02 18:37:03 +0300

    [FEATURE]: Implement updateUser endpoint and update response types
    Changes:
    - Added a new `@PatchMapping` endpoint for updating user information.
    - Updated `createUser` and `getUserById` to return `UserResponse` DTOs.
    - The `updateUser` method utilizes `UpdateUserRequest` for update data and returns `UserResponse`.
    
    Reasoning:
    - This commit introduces a functionality to update users through a dedicated endpoint.
    - Consistent use of `UserResponse` provides a structured response format for user data.

--- src/main/kotlin/com/charlottewiltshire0/updaterinstaller/api/controller/user/UserController.kt
+++ src/main/kotlin/com/charlottewiltshire0/updaterinstaller/api/controller/user/UserController.kt
@@ -1,9 +1,13 @@
 package com.charlottewiltshire0.updaterinstaller.api.controller.user
 
 import com.charlottewiltshire0.updaterinstaller.api.controller.dto.request.user.CreateUserRequest
+import com.charlottewiltshire0.updaterinstaller.api.controller.dto.request.user.UpdateUserRequest
+import com.charlottewiltshire0.updaterinstaller.api.controller.dto.responce.UserResponse
 import com.charlottewiltshire0.updaterinstaller.api.service.user.UserService
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,12 +22,25 @@ class UserController(
 ) {
 
     @PostMapping
-    fun createUser(@RequestBody body: CreateUserRequest) {
-        userService.createUser(body)
+    fun createUser(@RequestBody createUserRequest: CreateUserRequest): UserResponse {
+        return userService.createUser(createUserRequest)
     }
 
     @GetMapping("/{id}")
-    fun getUserById(@PathVariable id: Long) {
-        userService.getUserById(id)
+    fun getUserById(@PathVariable id: Long): UserResponse {
+        return userService.getUserById(id)
+    }
+
+    @PatchMapping("/{id}")
+    fun updateUser(
+        @PathVariable id: Long,
+        @RequestBody updateUserRequest: UpdateUserRequest
+    ): UserResponse {
+        return userService.updateUser(id, updateUserRequest)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteUserById(@PathVariable id: Long): String {
+        return userService.deleteUserById(id)
     }
 }
