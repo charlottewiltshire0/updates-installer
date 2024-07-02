15e51d8b9882bebed828cf9e41a47e82de064fcc
StarStudio <charlottewiltshire27@outlook.com>
2024-07-02 23:23:56 +0300

    [REFACTOR]: Remove Privileges entity and repository
    Changes:
    - Deleted the `Privileges.kt` entity class.
    - Deleted the `PrivilegesRepository.kt` repository interface.

--- src/main/kotlin/com/charlottewiltshire0/updaterinstaller/store/repositories/PrivilegesRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.charlottewiltshire0.updaterinstaller.store.repositories
-
-import com.charlottewiltshire0.updaterinstaller.store.entities.Privileges
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PrivilegesRepository : JpaRepository<Privileges, Long>
