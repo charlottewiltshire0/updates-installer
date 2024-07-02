9e2782e84df8aa7ce527318215779170070e288a
StarStudio <charlottewiltshire27@outlook.com>
2024-07-02 23:24:17 +0300

    [REFACTOR]: Rename Roles entity and repository to Role
    Changes:
    - Renamed the `Roles` entity class to `Role.kt`.
    - Renamed the `RolesRepository.kt` interface to `RoleRepository.kt`.
    - Updated entity annotations (singular table name, GenerationType.AUTO).
    - Introduced new fields: `createdAt` and `updatedAt` with appropriate annotations.
    - Modified the relationship between `Role` and `Privileges` (likely needs further investigation).

---
+++ src/main/kotlin/com/charlottewiltshire0/updaterinstaller/store/entities/Role.kt
@@ -0,0 +1,28 @@
+package com.charlottewiltshire0.updaterinstaller.store.entities
+
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "role")
+class Role (
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0,
+
+    @Column(unique = true, nullable = false)
+    val name: String,
+
+    @Column(nullable = true, length = 80)
+    val description: String,
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    val updatedAt: LocalDateTime = LocalDateTime.now()
+)
