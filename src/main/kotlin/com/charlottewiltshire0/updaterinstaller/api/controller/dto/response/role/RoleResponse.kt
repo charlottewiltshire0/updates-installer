a1ca459a5665c24c0eb4a2fda0f39a8ba9043cd7
StarStudio <charlottewiltshire27@outlook.com>
2024-07-03 00:37:12 +0300

    [UPDATED]: Enhanced RoleResponse DTO
    Changes:
    - Added data class `RoleResponse` to represent role information in API responses.
    - Included properties: `roleId` for unique identifier, `name` for role name, and `description` for role description.
    - Annotated properties with `@JsonProperty` for proper JSON serialization.

--- src/main/kotlin/com/charlottewiltshire0/updaterinstaller/api/controller/dto/response/role/RoleResponse.kt
+++ src/main/kotlin/com/charlottewiltshire0/updaterinstaller/api/controller/dto/response/role/RoleResponse.kt
@@ -1,4 +1,14 @@
 package com.charlottewiltshire0.updaterinstaller.api.controller.dto.response.role
 
-class RoleResponse {
-}
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class RoleResponse(
+    @JsonProperty("id")
+    val roleId: Long,
+
+    @JsonProperty("name")
+    val name: String,
+
+    @JsonProperty("description")
+    val description: String,
+)
