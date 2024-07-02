1b15110e08bac7ef708e5e3b19b6e1be4b6a0d9b
StarStudio <charlottewiltshire27@outlook.com>
2024-07-02 18:02:53 +0300

    REFACTOR]: Update UserService to use new DTOs for user creation
    Changes:
    - Updated the `createUser` method signature:
        - Replaced `RequestCreateUserDTO` with `CreateUserRequest`.
        - Replaced `ResponceCreateUserDTO` with `CreateUserResponse`.
    
    Reasoning:
    - This change aligns the service interface with the refactored DTO class names for improved consistency.

--- src/main/kotlin/com/charlottewiltshire0/updaterinstaller/api/service/user/UserService.kt
+++ src/main/kotlin/com/charlottewiltshire0/updaterinstaller/api/service/user/UserService.kt
@@ -1,9 +1,9 @@
 package com.charlottewiltshire0.updaterinstaller.api.service.user
 
-import com.charlottewiltshire0.updaterinstaller.api.controller.dto.request.user.RequestCreateUserDTO
-import com.charlottewiltshire0.updaterinstaller.api.controller.dto.responce.ResponceCreateUserDTO
+import com.charlottewiltshire0.updaterinstaller.api.controller.dto.request.user.CreateUserRequest
+import com.charlottewiltshire0.updaterinstaller.api.controller.dto.responce.CreateUserResponse
 
 interface UserService {
 
-    fun createUser(requestCreateUserDTO: RequestCreateUserDTO): ResponceCreateUserDTO
+    fun createUser(createUserRequest: CreateUserRequest): CreateUserResponse
 }
