ebd9e62eaf494b9eb3bde04e7a662697c933ff75
StarStudio <charlottewiltshire27@outlook.com>
2024-07-02 19:14:16 +0300

    cleaned

--- src/main/kotlin/com/charlottewiltshire0/updaterinstaller/store/repositories/UserRepository.kt
+++ src/main/kotlin/com/charlottewiltshire0/updaterinstaller/store/repositories/UserRepository.kt
@@ -1,10 +1,8 @@
 package com.charlottewiltshire0.updaterinstaller.store.repositories
 
 import com.charlottewiltshire0.updaterinstaller.store.entities.User
-import org.springframework.data.domain.Page
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.awt.print.Pageable
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
