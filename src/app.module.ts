65a97c14157c0358b9c5e72019eae29e3422c1a5
StarStudio <charlottewiltshire27@outlook.com>
2024-08-04 17:48:54 +0300

    [PATCH] [FEATURE]: Add database entities for User, Ticket, and Device
    - Created `UserEntity` for user management with roles and relationships.
    - Added `TicketEntity` to represent tickets with relationships to users and devices.
    - Introduced `DeviceEntity` for device information with relationships to users and tickets.
    
    Changes include:
    - `UserEntity` with fields for username, email, password, role, and associated devices and tickets.
    - `TicketEntity` with fields for installer ID, user ID, and device ID.
    - `DeviceEntity` with fields for device type, model, OS version, platform, and architecture.
    
    Also updated:
    - `UserModule` now imports `ManagementModule` and `TicketsModule`.
    - Renamed and updated `typeorm.config.ts` to `app.module.ts` to configure `TypeOrmModule` for PostgreSQL integration and environment-based configuration.

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ApplicationModule } from './application/application.module';
+import { UpdatesModule } from './updates/updates.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ApplicationModule,
+    UpdatesModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
