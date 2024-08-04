41400b2e0912f875cc53a4ec4df5b81da91329a5
StarStudio <charlottewiltshire27@outlook.com>
2024-08-04 19:29:43 +0300

    [UPDATE]: Integrate TypeOrmModule and UserService into ManagementModule
    Changes:
    - Imported `TypeOrmModule` and configured it with `UserEntity` in `ManagementModule`.
    - Added `UserService` to the `providers` array in `ManagementModule`.

--- src/user/management/management.module.ts
+++ src/user/management/management.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './controller/user/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../../database/entities/user.entity';
+import { UserService } from './service/user/user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
+  providers: [UserService],
 })
 export class ManagementModule {}
